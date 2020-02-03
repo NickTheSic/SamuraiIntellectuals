110e4e84baa8ece26c8183f49213c9772d0390bb
Nick <lemm0016@algonquinlive.com>
2020-02-03 18:38:59 -0500

    Started on the menu system for the tower shop

---
+++ Source/SI_JustSurvive/UI/TowerShopButton.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "TowerShopButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SI_JUSTSURVIVE_API UTowerShopButton : public UButton
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SI_JustSurvive/UI/TowerShopMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TowerShopMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SI_JUSTSURVIVE_API UTowerShopMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
