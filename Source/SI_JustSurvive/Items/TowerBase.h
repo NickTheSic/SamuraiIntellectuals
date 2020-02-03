e8644058248f92a5943d7e5ceba2c183e3fb1a36
Vanessa R <nesacr@hotmail.com>
2020-02-03 18:43:50 -0500

    Test Commit to TowerBase

---
+++ Source/SI_JustSurvive/Items/TowerBase.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase.h"
+#include "TowerBase.generated.h"
+
+USTRUCT()
+struct FTowerData
+{
+	GENERATED_USTRUCT_BODY()
+
+		UPROPERTY()
+		int m_Damage;
+
+	/* UPROPERTY()
+		 EElementType m_DamageType;*/
+
+	UPROPERTY()
+		int m_FireRate;
+
+	UPROPERTY()
+		float m_TowerRange;
+
+	UPROPERTY()
+		int m_ResellValue;
+
+	UPROPERTY()
+		int m_UpgradeCost;
+
+	/*UPROPERTY()
+		ETowerType m_TowerType;*/
+
+};
+
+/**
+ * 
+ */
+UCLASS()
+class SI_JUSTSURVIVE_API ATowerBase : public AItemBase
+{
+	GENERATED_BODY()
+
+public:
+	FTowerData m_TowerData;
+
+	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* TowerMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		class UPawnSensingComponent* m_PawnSensingComp;
+
+	ATowerBase();
+
+protected:
+
+	
+};
