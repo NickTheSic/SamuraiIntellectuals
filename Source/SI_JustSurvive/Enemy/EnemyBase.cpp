06049bcfbe7905b985b221161a917adf997fff95
Nick <nicklemmon@live.ca>
2020-02-09 13:34:35 -0500

    Added a waypoint manager so we have one pointer to give to the enemies that can deal with the actual groups.  Didn't get working what I thought I would
    Signed-off-by: Nick <nicklemmon@live.ca>

--- Source/SI_JustSurvive/Enemy/EnemyBase.cpp
+++ Source/SI_JustSurvive/Enemy/EnemyBase.cpp
@@ -3,12 +3,26 @@
 
 #include "EnemyBase.h"
 
+#include "../Waypoints/Waypoint.h"
+#include "../Waypoints/WaypointGroup.h"
+#include "../Waypoints/WaypointManager.h"
+
 // Sets default values
 AEnemyBase::AEnemyBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+}
+
+void AEnemyBase::SetWaypointManager(AWaypointManager* wayMan)
+{
+	check(wayMan && "Waypoint Manager was null");
+
+	if (wayMan)
+	{
+		m_WaypointManager = wayMan;
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/SI_JustSurvive/Enemy/EnemyBase.h
+++ Source/SI_JustSurvive/Enemy/EnemyBase.h
@@ -8,9 +8,30 @@
 
 /*
 *	Change Log:
-*
-*			2/8/2020: Nick: Initial Creation.  I am not sure how to do behaviour trees
+*			
+			2/9/2020: Nick - I added a Waypoint manager
+*			2/8/2020: Nick - Initial Creation.  I am not sure how to do behaviour trees
 */
+
+USTRUCT()
+struct SI_JUSTSURVIVE_API FEnemyData
+{
+	GENERATED_BODY()
+
+	//TODO: Look the Damage type stuff
+	// I Think health is part of TakeDamage
+	UPROPERTY(EditAnywhere, Category = "Config")
+		float EnemyMaxHealth;
+
+	UPROPERTY(EditAnywhere, Category = "Config")
+		float CurrentHealth;
+
+	//UPROPERTY(EditAnywhere, Category = "Config")
+	//	Weapon or Inventory 
+	
+	UPROPERTY(EditAnywhere, Category = "Config")
+		class UStaticMesh* m_EnemyMesh;//TODO: Check if we need a Static or Skeletal and also set it here
+};
 
 UCLASS()
 class SI_JUSTSURVIVE_API AEnemyBase : public AActor
@@ -21,9 +42,24 @@ public:
 	// Sets default values for this actor's properties
 	AEnemyBase();
 
+	//To be set when the enemy is spawned
+	void SetWaypointManager(class AWaypointManager* wayMan);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Config" ) //Todo: Make this in protected somehow? With allow private access
+		class UStaticMeshComponent* m_EnemyMesh; //TODO: Maybe make this a Skeletal Mesh
+
+	UPROPERTY(EditAnywhere, Category = "Enemy Config")
+		FEnemyData EnemyData;
+
+	class AWaypointManager* m_WaypointManager = nullptr;
+
+	//TODO: Add a Get new Waypoint function
+	
+	class AWaypoint* m_TargetWaypoint = nullptr;
 
 public:	
 	// Called every frame
