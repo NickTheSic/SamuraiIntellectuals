463ee1b845f2ab41ad10cb02d594080f7f4bfa2e
Vanessa R <nesacr@hotmail.com>
2020-02-10 19:32:57 -0500

    Generator class added

---
+++ Source/SI_JustSurvive/Items/GeneratorBase.h
@@ -0,0 +1,40 @@
+// Copyright 2020 Samurai Intellectuals
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GeneratorBase.generated.h"
+
+UCLASS()
+class SI_JUSTSURVIVE_API AGeneratorBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGeneratorBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+//ADDED START
+	UFUNCTION()
+		void TakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageInflictor);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "DamageMesh")
+		class USceneComponent* GeneratorRoot;
+
+	UPROPERTY(VisibleAnywhere, BluepringReadWrite, Category = "DamageMesh")
+		class UStaticMeshComponent* GeneratorBody;
+
+	int m_HP = 100;
+
+//ADDED END
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
