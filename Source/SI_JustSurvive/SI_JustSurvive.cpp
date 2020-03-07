c1476259155da269ae7d1504893d6b1a83cbf643
Anthony D'Alessandro <dale0135@algonquinlive.com>
2020-03-06 22:08:41 -0500

    commiting

--- Source/SI_JustSurvive/SI_JustSurvive.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "SI_JustSurvive.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SI_JustSurvive, "SI_JustSurvive" );
- 
